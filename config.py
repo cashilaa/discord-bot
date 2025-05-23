01f78905ce4d4a067c49a9ff77fa1ee59aab2c94
sheila wafula <167983359+cashilaa@users.noreply.github.com>
2025-05-23 13:16:48 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-# Discord Bot Configuration
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token once you have it
-TOKEN = 'MTM3NTM0NTAwODA4NTA0MTE2Mg.GP803U.ID5qmEQfBDSVRo8Rn5uBhxETHrZRnL4cQPeX_8'
